1267ae852708e67a27ec7f385fcc6f21dd7481c5
Adeel Hamid <adeel.hamid50@gmail.com>
2025-09-10 22:57:17 +1000

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Adeel Hamid",
+    author_email="adeel.hamid50@gmail.com",
+    packages=find_packages()
+)
